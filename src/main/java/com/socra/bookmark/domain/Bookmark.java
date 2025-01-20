ddf953cfcbace0eccb6b75b3f49f73436dbc983b
socra167 <socra167@gmail.com>
2025-01-20 17:39:32 +0900

    :sparkles: feat: Bookmark, CustomTag 연관관계 수정

--- src/main/java/com/socra/bookmark/domain/Bookmark.java
+++ src/main/java/com/socra/bookmark/domain/Bookmark.java
@@ -2,8 +2,6 @@ package com.socra.bookmark.domain;
 
 import java.net.URI;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,7 +12,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -44,9 +41,6 @@ public class Bookmark {
 	@CreatedDate
 	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdDate;
-
-	@OneToMany(mappedBy = "bookmarks")
-	private List<CustomTag> customTag = new ArrayList<>();
 
 	@Builder
 	public Bookmark(String name, URI uri, String description) {
