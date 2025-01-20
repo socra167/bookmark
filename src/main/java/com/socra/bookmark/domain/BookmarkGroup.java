1c643451b98de269534d4f702ea25aa07f018ae5
socra167 <socra167@gmail.com>
2025-01-20 18:15:04 +0900

    :sparkles: feat: BookmarkGroup createdDate 추가

--- src/main/java/com/socra/bookmark/domain/BookmarkGroup.java
+++ src/main/java/com/socra/bookmark/domain/BookmarkGroup.java
@@ -1,10 +1,15 @@
 package com.socra.bookmark.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,6 +19,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -26,6 +32,10 @@ public class BookmarkGroup {
 
 	@Column(nullable = false)
 	private String name;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdDate;
 
 	@OneToMany
 	private List<Bookmark> bookmarks = new ArrayList<>();
