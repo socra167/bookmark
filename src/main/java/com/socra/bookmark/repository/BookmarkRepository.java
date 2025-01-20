9d0d8b8a91f69316f31f95265a258c562627ab7b
socra167 <socra167@gmail.com>
2025-01-20 16:29:44 +0900

    :fire: chore: 불필요한 메서드 제거

--- src/main/java/com/socra/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/socra/bookmark/repository/BookmarkRepository.java
@@ -4,17 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Optional;
 
 import com.socra.bookmark.domain.Bookmark;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-
-    Optional<Bookmark> findById(Long id);
-    boolean existsById(Long id);
-    List<Bookmark> findAll();
     boolean existsByUri(URI uri);
     void deleteByUri(URI uri);
 }
