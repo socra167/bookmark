086b85c85a45f81d2a47085888020cfca077eced
socra167 <socra167@gmail.com>
2025-01-20 17:12:49 +0900

    :recycle: refactor: `Bookmark` 테이블명 변경

--- src/main/java/com/socra/bookmark/domain/Bookmark.java
+++ src/main/java/com/socra/bookmark/domain/Bookmark.java
@@ -8,14 +8,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Data
+@Table(name = "bookmarks")
 @Entity
 public class Bookmark {
 	@Id
