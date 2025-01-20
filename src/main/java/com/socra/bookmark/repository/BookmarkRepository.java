0f7539e3546eaae6bf3917a6659c89ee4ff99404
socra167 <socra167@gmail.com>
2025-01-20 16:30:22 +0900

    :lipstick: style: 라인 포매팅

--- src/main/java/com/socra/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/socra/bookmark/repository/BookmarkRepository.java
@@ -1,14 +1,17 @@
 package com.socra.bookmark.repository;
 
+import java.net.URI;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.net.URI;
 
 import com.socra.bookmark.domain.Bookmark;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    boolean existsByUri(URI uri);
-    void deleteByUri(URI uri);
+
+	boolean existsByUri(URI uri);
+
+	void deleteByUri(URI uri);
+
 }
