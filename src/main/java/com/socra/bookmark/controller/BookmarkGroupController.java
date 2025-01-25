132d7aaa7d0ff0fc30d03db00cdcf140794de7fc
socra167 <socra167@gmail.com>
2025-01-25 19:54:56 +0900

    :fire: style: import 정리

--- src/main/java/com/socra/bookmark/controller/BookmarkGroupController.java
+++ src/main/java/com/socra/bookmark/controller/BookmarkGroupController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
